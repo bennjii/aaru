7b7b16218591cc4f5bce430dbbd26313bd45a703
Ben White <contact@bennjii.dev>
2024-11-10 14:18:48 +1100

    mod: cherrypick for another PR

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 #![doc = include_str!("../docs/head.md")]
 #![allow(dead_code)]
 
-use mimalloc::MiMalloc;
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
-
 #[cfg(feature = "codec")]
 use crate::codec::error::CodecError;
 use crate::geo::error::GeoError;
