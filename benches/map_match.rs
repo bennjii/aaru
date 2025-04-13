081e6500201c62a57289669a7f8bfcc7fe0960cb
Ben White <contact@bennjii.dev>
2025-04-13 20:15:11 +1000

    mod: restructure bench & include relevant files

--- benches/map_match.rs
+++ benches/map_match.rs
@@ -1,4 +1,6 @@
-use aaru::codec::consts::{LOS_ANGELES, VENTURA_MATCHED, VENTURA_TRIP};
+use aaru::codec::consts::{
+    LAX_LYNWOOD_MATCHED, LAX_LYNWOOD_TRIP, LOS_ANGELES, VENTURA_MATCHED, VENTURA_TRIP, ZURICH,
+};
 use aaru::route::Graph;
 use criterion::criterion_main;
 use geo::{coord, LineString};
@@ -7,52 +9,71 @@ use wkt::{ToWkt, TryFromWkt};
 
 struct MapMatchScenario {
     name: &'static str,
-    source_file: &'static str,
-
     input_linestring: &'static str,
     expected_linestring: &'static str,
 }
 
-const MATCH_CASES: [MapMatchScenario; 1] = [MapMatchScenario {
-    name: "VENTURA_HWY",
-    source_file: LOS_ANGELES,
+struct GraphArea {
+    source_file: &'static str,
+    matches: &'static [MapMatchScenario],
+}
 
-    input_linestring: VENTURA_TRIP,
-    expected_linestring: VENTURA_MATCHED,
-}];
+const MATCH_CASES: [GraphArea; 2] = [
+    GraphArea {
+        source_file: LOS_ANGELES,
+        matches: &[
+            MapMatchScenario {
+                name: "VENTURA_HWY",
+                input_linestring: VENTURA_TRIP,
+                expected_linestring: VENTURA_MATCHED,
+            },
+            MapMatchScenario {
+                name: "LAX_LYNWOOD",
+                input_linestring: LAX_LYNWOOD_TRIP,
+                expected_linestring: LAX_LYNWOOD_MATCHED,
+            },
+        ],
+    },
+    GraphArea {
+        source_file: ZURICH,
+        matches: &[],
+    },
+];
 
 fn target_benchmark(c: &mut criterion::Criterion) {
     let mut group = c.benchmark_group("match");
     group.significance_level(0.1).sample_size(30);
 
-    MATCH_CASES.into_iter().for_each(|sc| {
-        let path = Path::new(sc.source_file).as_os_str().to_ascii_lowercase();
+    MATCH_CASES.into_iter().for_each(|ga| {
+        let path = Path::new(ga.source_file).as_os_str().to_ascii_lowercase();
         let graph = Graph::new(path).expect("Graph must be created");
 
-        group.bench_function(format!("match: {}", sc.name), |b| {
-            b.iter(|| {
-                let coordinates: LineString<f64> =
-                    LineString::try_from_wkt_str(sc.input_linestring)
-                        .expect("Linestring must parse successfully.");
+        ga.matches.iter().for_each(|sc| {
+            group.bench_function(format!("match: {}", sc.name), |b| {
+                b.iter(|| {
+                    let coordinates: LineString<f64> =
+                        LineString::try_from_wkt_str(sc.input_linestring)
+                            .expect("Linestring must parse successfully.");
 
-                let result = graph
-                    .map_match(coordinates)
-                    .expect("Match must complete successfully");
+                    let result = graph
+                        .map_match(coordinates)
+                        .expect("Match must complete successfully");
 
-                let linestring = result
-                    .matched()
-                    .iter()
-                    .map(|node| {
-                        coord! {
-                            x: node.position.x(),
-                            y: node.position.y(),
-                        }
-                    })
-                    .collect::<LineString>();
+                    let linestring = result
+                        .matched()
+                        .iter()
+                        .map(|node| {
+                            coord! {
+                                x: node.position.x(),
+                                y: node.position.y(),
+                            }
+                        })
+                        .collect::<LineString>();
 
-                let as_wkt_string = linestring.wkt_string();
-                assert_eq!(as_wkt_string, sc.expected_linestring);
-            })
+                    let as_wkt_string = linestring.wkt_string();
+                    assert_eq!(as_wkt_string, sc.expected_linestring);
+                })
+            });
         });
     });
 
