4017d45feddd321b40c22570ff653b84b07596de
Ben White <contact@bennjii.dev>
2025-06-08 16:30:44 +1000

    mod(docs): fill in doc comments

--- libs/routers_codec/src/osm/parsers/speed_limit/restriction.rs
+++ libs/routers_codec/src/osm/parsers/speed_limit/restriction.rs
@@ -3,12 +3,14 @@ use std::str::FromStr;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Restriction {
-    /// ...
+    /// The transport mode by which the user is travelling.
+    /// This may be omitted if not specified, therefore optional.
     ///
     /// See: https://wiki.openstreetmap.org/wiki/Key:access#Transport_mode_restrictions
     pub(crate) transport_mode: Option<TransportMode>,
 
-    /// ...
+    /// The directionality limit from which the user is travelling.
+    /// Can be used to limit roadways which only permit travel in particular directions.
     ///
     /// See: https://wiki.openstreetmap.org/wiki/Forward_%26_backward,_left_%26_right
     pub(crate) directionality: Option<Directionality>,
